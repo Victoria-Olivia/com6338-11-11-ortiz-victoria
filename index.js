3cdd22cc4d79de1d680c49f0f2d3433991b0cbdd
Victoria Ortiz <ortiz.victoria@ufl.edu>
2025-08-03 07:11:38 -0400

    Completed makePoemHTML function to display poem title, author, and stanzas

--- index.js
+++ index.js
@@ -9,7 +9,34 @@ const pipe = (...fns) => firstArg => fns.reduce((returnValue, fn) => fn(returnVa
 const makeTag = tag => str => `<${tag}>${str}</${tag}>`
 
 // complete this function
-const makePoemHTML = () => {}
+const makePoemHTML = function(poemData) {
+  const poem = poemData[0]
+  const title = poem.title
+  const author = poem.author
+  const lines = poem.lines
+  const makeH2 = makeTag('h2')
+  const makeH3 = makeTag('h3')
+  const makeP = makeTag('p')
+  const makeEm = makeTag('em')
+
+  const byAuthor = name => `by ${name}`
+  const authorIntroLine = pipe(byAuthor, makeEm, makeH3)
+  const stanzas = []
+  let currentStanza = []
+  lines.forEach(line => {
+    if (line === '') {
+      stanzas.push(currentStanza)
+      currentStanza = []
+    } else {
+      currentStanza.push(line)
+    }
+  })
+  if (currentStanza.length > 0) {
+    stanzas.push(currentStanza)
+  }
+  const paragraphHTML = stanzas.map(stanza => makeP(stanza.join('<br>'))).join('');
+  return makeH2(title) + authorIntroLine(author) + paragraphHTML
+}
 
 // attach a click event to #get-poem
 getPoemBtn.onclick = async function() {
